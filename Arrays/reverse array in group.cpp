08d8a3044a6893119d5f9c4fcea889a6a3170016
rajkumargrs113 <rajkumarguptars113@gmail.com>
2024-07-09 23:31:37 +0530

    add in arrays

---
+++ Arrays/reverse array in group.cpp
@@ -0,0 +1,13 @@
+void reverseInGroups(vector<long long int> &arr, int k) {
+        // code here
+        
+        int start=0;
+        while(start<arr.size())
+        {
+            if(start+k<arr.size())
+                reverse(arr.begin()+start,arr.end()+start+k);
+            else
+                reverse(arr.begin()+start,arr.end());
+            start+=k;
+        }
+    }
